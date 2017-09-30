91e9dd4d87ca1b5b172bd354e1d6302b315fdeba
harrisondowns <harrisonmdowns@gmail.com>
2017-09-30 11:57:35 -0400

    Added socket class for easy communication on iPhone ports using read() and write() functions.

---
+++ proxSpeaker/Sockpuppet.swift
@@ -0,0 +1,60 @@
+//
+//  Sockpuppet.swift
+//  proxSpeaker
+//
+//  Created by Groot Groot on 9/30/17.
+//  Copyright © 2017 Harrison Downs. All rights reserved.
+//
+
+import Foundation
+
+class Sockpuppet {
+    private var addr : String
+    private var port : Int
+    private var buffer : [UInt8]
+    private var inp : InputStream?
+    private var out : OutputStream?
+    private var inputStream : InputStream!
+    private var outputStream : OutputStream!
+    
+    
+    init(a : String, p : Int){
+        addr = a
+        port = p
+        buffer = [UInt8]()
+        
+        Stream.getStreamsToHost(withName: addr, port: port, inputStream: &inp, outputStream: &out)
+        inputStream = inp!
+        outputStream = out!
+        inputStream.open()
+        outputStream.open()
+        
+        
+    }
+
+    func write(outputString : String){
+        let asUInt8Array = String(outputString.characters).utf8.map{ UInt8($0) }
+        for c in asUInt8Array{
+            buffer.append(c);
+        }
+        outputStream.write(&buffer, maxLength: buffer.count)
+        
+        buffer.removeAll()
+    }
+    
+    func read() -> String{
+        var readByte :UInt8 = 0
+        var buff = [UInt8]()
+        while inputStream.hasBytesAvailable {
+            inputStream.read(&readByte, maxLength: 1)
+            buff.append(readByte)
+        }
+        
+        return String(bytes: buff, encoding: String.Encoding.utf8)!
+        
+    }
+   
+    
+
+    
+}
--- proxSpeaker/ViewController.swift
+++ proxSpeaker/ViewController.swift
@@ -7,20 +7,50 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    
+    
+    var sock : Sockpuppet!
+    
+    func startSocket(){
+        sock = Sockpuppet(a: "127.0.0.1", p: 56565)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }//
+        startSocket()
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    // rip rip rip
+    func socketsForDays(){
+        let addr = "127.0.0.1"
+        let port = 56565
+        
+      
+        var inp :InputStream?
+        var out :OutputStream?
+        var buffer : [UInt8] = [20, 32, 64, 80]
+        Stream.getStreamsToHost(withName: addr, port: port, inputStream: &inp, outputStream: &out)
+        let inputStream = inp!
+        let outputStream = out!
+        inputStream.open()
+        outputStream.open()
+        
+        var readByte :UInt8 = 0
+        while inputStream.hasBytesAvailable {
+            inputStream.read(&readByte, maxLength: 1)
+        }
+        
+        // buffer is a UInt8 array containing bytes of the string "Jonathan Yaniv.".
+        outputStream.write(&buffer, maxLength: buffer.count)
+    }
     
 
 
